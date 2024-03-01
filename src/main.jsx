800589d8594a14172598e08d9e6cc3262aef1f17
[ Tanitphon Paniwan ] <lw91945@linecorp.com>
2024-03-01 23:05:33 +0700

    add redirect router

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, Navigate } from 'react-router-dom'
+import { AuthProvider, AuthContext } from './contexts/AuthContext.jsx'
 
-import { AuthProvider } from './contexts/AuthContext.jsx'
 import Login from './pages/Login.jsx'
 import Profile from './pages/Profile.jsx'
 import Register from './pages/Register.jsx'
 
 import './index.css'
 
+const ProtectedRoute = ({ children }) => {
+  const { user, checkingStatus } = useContext(AuthContext)
+
+  if (checkingStatus) return <div>Loading...</div> // Show loading state
+
+  if (user) return <Navigate to="/profile" replace />
+
+  return children
+}
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Login />,
+    element: (
+      <ProtectedRoute>
+        <Login />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/register',
+    element: (
+      <ProtectedRoute>
+        <Register />
+      </ProtectedRoute>
+    ),
   },
   {
     path: '/profile',
     element: <Profile />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
   },
 ])
 
