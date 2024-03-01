eca6011997b24cd482daaa949b3ee3aa5a55ec87
[ Tanitphon Paniwan ] <lw91945@linecorp.com>
2024-03-01 10:39:01 +0700

    add register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { auth } from '../firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
 function Register() {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const registerUser = async (e) => {
+    e.preventDefault()
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      console.log(userCredential.user) // You might want to do something with the user info or redirect
+      alert('Register success')
+      navigate('/profile') // Redirect to profile page after successful registration
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
   return (
     <div className="flex items-center justify-center h-screen bg-gray-200">
       <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
         <h3 className="text-2xl font-bold text-center">Register</h3>
-        <p className="text-center text-red-500"> Error Message</p>
-        <form>
+        {error && <p className="text-red-500">{error}</p>}
+        <form onSubmit={registerUser}>
           <div className="mt-4">
             <div>
               <label className="block" htmlFor="email">
@@ -13,6 +40,7 @@ function Register() {
               <input
                 type="email"
                 placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
                 required
               />
@@ -22,6 +50,7 @@ function Register() {
               <input
                 type="password"
                 placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
                 required
               />
